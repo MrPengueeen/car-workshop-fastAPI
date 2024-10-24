a12ffe2e3c93d4bc3cc664bf48474f408e93831f
Aonmoy <aonmoy0.0@gmail.com>
2024-10-25 01:39:15 +0600

    fix: change booking query by date range API's input from query parameter to request body

--- src/routers/bookings.py
+++ src/routers/bookings.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Body
 from sqlalchemy.orm import Session
 import schemas, crud
 import dependencies
 import datetime
+from typing import Annotated
 
 router = APIRouter(
     prefix="/api/v1/bookings",
@@ -17,7 +18,7 @@ async def get_bookings(mechanic_id: int | None = None, skip: int=0, limit: int=1
     return crud.get_bookings_by_mechanic(db=db, mechanic_id=mechanic_id, skip=skip, limit=limit)
 
 @router.post("/range", response_model=list[schemas.Booking])
-async def get_bookings_by_date_range(start: datetime.datetime | None = None, end: datetime.datetime | None = None, db: Session=Depends(dependencies.get_db)):
+async def get_bookings_by_date_range(start: Annotated[datetime.datetime, Body(...)], end: Annotated[datetime.datetime, Body(...)], db: Session=Depends(dependencies.get_db)):
     return crud.get_bookings_by_date_range(db=db, start=start, end=end)
 
 
